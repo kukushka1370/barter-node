f2aaad54b556a0996aadf2c5bffe372ea365ba2d
shheeeeshka <102031292+shheeeeshka@users.noreply.github.com>
2024-10-20 15:46:14 +0300

    3uf93f9

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,11 +2,22 @@ import { Router } from "express";
 
 import userController from "../controllers/userController.js";
 import checkAuth from "../middleware/authMiddleware.js";
+import { User } from "../models/user-model.js";
 
 const router = new Router();
 
 router.get('/', userController.getAllUsers); // checkAuth
 router.post('/set/app-or-decline', userController.approveOrDecline); // checkAuthrating, userId, review
 router.post('/update-rating', userController.updateUserRating); // checkAuthrating, userId, review
+
+router.get('/find/:userId', async (req, res, next) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findById(userId);
+        return res.json(user);
+    } catch (err) {
+        next(err);
+    }
+});
 
 export default router;
